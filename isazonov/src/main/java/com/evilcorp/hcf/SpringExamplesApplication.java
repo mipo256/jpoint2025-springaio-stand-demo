6712cf1d53bf431e5f073ed5397e8225219f5e2c
Ilia Sazonov <poxvuibr@gmail.com>
2025-04-01 14:49:29 +0300

    Rename the application

--- isazonov/src/main/java/com/evilcorp/hcf/SpringExamplesApplication.java
+++ isazonov/src/main/java/com/evilcorp/hcf/SpringExamplesApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernateCustomFunctionsApplication {
+public class SpringExamplesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HibernateCustomFunctionsApplication.class, args);
+        SpringApplication.run(SpringExamplesApplication.class, args);
     }
 }
